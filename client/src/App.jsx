c1c3e019ae596deffbba1bb1f976a3b1513a85d4
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-01-10 00:04:45 +0530

    complete signup page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route paht="/login" element={<LogIn />} />
+        <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />

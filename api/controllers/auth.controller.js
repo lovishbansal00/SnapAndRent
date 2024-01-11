495af4238cf02f42dbd83ffc4b5464d649d9999f
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-01-12 00:56:14 +0530

    created photo upload functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,16 @@
-import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
-import jwt from "jsonwebtoken";
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
 
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUSer = new User({ username, email, password: hashedPassword });
-
+    const newUser = new User({ username, email, password: hashedPassword });
     try {
-        await newUSer.save();
-        res.status(201).json("user created successfully");
+        await newUser.save();
+        res.status(201).json('User created successfully!');
     }
     catch (err) {
         next(err);
@@ -25,20 +24,20 @@ export const signin = async (req, res, next) => {
         const validUser = await User.findOne({ email });
         if (!validUser) {
             return next(errorHandler
-                (404, "User not found"));
+                (404, 'User not found!'));
         }
 
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) {
             return next(errorHandler
-                (401, "Invalid password"));
+                (401, 'Wrong credentials!'));
         }
 
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: pass, ...rest } = validUser._doc;
 
         res
-            .cookie('access token', token, { httpOnly: true })
+            .cookie('access_token', token, { httpOnly: true })
             .status(200)
             .json(rest);
     } catch (error) {
@@ -60,7 +59,7 @@ export const google = async (req, res, next) => {
         else {
             const generatePassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
             const hashedPassword = bcryptjs.hashSync(generatePassword, 10);
-            const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword, avatar: req.body.photo });
+            const newUser = new User({ username: req.body.name.split(' ').join('').toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword, avatar: req.body.photo });
 
             await newUser.save();
             const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,5 +1,5 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import { app } from "../firebase,js";
+import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { loginSuccess } from "../redux/user/userSlice";
 import { useNavigate } from "react-router-dom";
@@ -14,7 +14,7 @@ export default function OAuth() {
 
       const result = await signInWithPopup(auth, provider);
 
-      const res = await fetch("api/auth/google", {
+      const res = await fetch("/api/auth/google", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

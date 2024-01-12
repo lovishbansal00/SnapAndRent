45680fda5f26020298805f2295fa1bf76d767474
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-01-12 16:03:25 +0530

    create user update api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,9 @@
 import express from 'express';
+import { updateUSer } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-    res.json({
-        message: "Hello World",
-    })
-})
+router.post("/update/:id", verifyToken, updateUSer);
 
 export default router;

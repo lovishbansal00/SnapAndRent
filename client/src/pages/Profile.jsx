3ae76718c2e5d460d12e58c0adbbcc4fbcfab29e
lovishbansal00 <lovish.bansal.330@gmail.com>
2024-01-24 19:25:21 +0530

    add show user listings funcitonality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -29,6 +29,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 
   useEffect(() => {
     if (file) {
@@ -120,6 +122,21 @@ export default function Profile() {
     }
   };
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7 ">Profile</h1>
@@ -201,6 +218,33 @@ export default function Profile() {
       <p className="text-green-700 mt-5">
         {updateSuccess ? "Profile updated successfully!" : ""}
       </p>
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingsError ? "Error showing listings" : ""}
+      </p>
+      {userListings &&
+        userListings.length > 0 &&
+        userListings.map((listing) => {
+          <div
+            key={listing._id}
+            className="border rounded-lg p-3 flex justify-between items-center"
+          >
+            <Link to={`/listing/${listing._id}`}>
+              <img
+                src={listing.imageUrls[0]}
+                alt="listing image cover"
+                className="h-16 w-16 object-contain"
+              />
+            </Link>
+            <Link to={`/listing/${listing._id}`}>
+              <p className="text-slate-700 font-semibold flex-1 hover:underline truncate">
+                {listing.name}
+              </p>
+            </Link>
+          </div>;
+        })}
     </div>
   );
 }
